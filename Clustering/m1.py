a7ee7fa0bfc114ad89f98b5c346db619f49bb4a3
rishabhshetty <rishabhshettyvm@gmail.com>
2019-05-19 23:09:02 +0530

    neural networks

---
+++ Clustering/m1.py
@@ -0,0 +1,120 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+import numpy as np
+
+X = np.array([[1, 2],
+              [1.5, 1.8],
+              [5, 8 ],
+              [8, 8],
+              [1, 0.6],
+              [9,11],
+              [8,2],
+              [10,2],
+              [9,3],])
+
+##plt.scatter(X[:,0], X[:,1], s=150)
+##plt.show()
+
+colors = 10*["g","r","c","b","k"]
+
+class Mean_Shift:
+    def __init__(self, radius=4):
+        self.radius = radius
+
+    def fit(self, data):
+        centroids = {}
+
+        for i in range(len(data)):
+            centroids[i] = data[i]
+        
+        while True:
+            new_centroids = []
+            for i in centroids:
+                in_bandwidth = []
+                centroid = centroids[i]
+                for featureset in data:
+                    if np.linalg.norm(featureset-centroid) < self.radius:
+                        in_bandwidth.append(featureset)
+
+                new_centroid = np.average(in_bandwidth,axis=0)
+                new_centroids.append(tuple(new_centroid))
+
+            uniques = sorted(list(set(new_centroids)))
+
+            prev_centroids = dict(centroids)
+
+            centroids = {}
+            for i in range(len(uniques)):
+                centroids[i] = np.array(uniques[i])
+
+            optimized = True
+
+            for i in centroids:
+                if not np.array_equal(centroids[i], prev_centroids[i]):
+                    optimized = False
+                if not optimized:
+                    break
+                
+            if optimized:
+                break
+
+        self.centroids = centroids
+
+
+
+clf = Mean_Shift()
+clf.fit(X)
+
+centroids = clf.centroids
+
+plt.scatter(X[:,0], X[:,1], s=150)
+
+for c in centroids:
+    plt.scatter(centroids[c][0], centroids[c][1], color='k', marker='*', s=150)
+
+plt.show()
+
+    def __init__(self, radius=None, radius_norm_step = 100):
+        self.radius = radius
+        self.radius_norm_step = radius_norm_step
+So the plan here is to create a massive radius, but make that radius go in steps, like bandwidths, or a bunch of radiuses with different lengths, which we'll call steps. If a featureset is in the closest radius, it will have a much higher "weight" than one much further away. The only question is what those steps ought to be! Now, beginning our fit method:
+
+    def fit(self, data):
+
+        if self.radius == None:
+            all_data_centroid = np.average(data, axis=0)
+            all_data_norm = np.linalg.norm(all_data_centroid)
+            self.radius = all_data_norm / self.radius_norm_step
+
+        centroids = {}
+
+        for i in range(len(data)):
+            centroids[i] = data[i]
+Here, if the user hasn't hard-coded the radius, then we're going to find the "center" of ALL of the data. Then, we will take the norm of that data, then we say each radius with self.radius is basically the full data-length, divided by how many steps we wanted to have. From here the centroids definitions remain the same as before in the above code. Now we begin the optimization while loop:
+
+        weights = [i for i in range(self.radius_norm_step)][::-1]    
+        while True:
+            new_centroids = []
+            for i in centroids:
+                in_bandwidth = []
+                centroid = centroids[i]
+                
+                for featureset in data:
+                    #if np.linalg.norm(featureset-centroid) < self.radius:
+                    #    in_bandwidth.append(featureset)
+                    distance = np.linalg.norm(featureset-centroid)
+                    if distance == 0:
+                        distance = 0.00000000001
+                    weight_index = int(distance/self.radius)
+                    if weight_index > self.radius_norm_step-1:
+                        weight_index = self.radius_norm_step-1
+
+                    to_add = (weights[weight_index]**2)*[featureset]
+                    in_bandwidth +=to_add
+                    
+
+                new_centroid = np.average(in_bandwidth,axis=0)
+                new_centroids.append(tuple(new_centroid))
+
+            uniques = sorted(list(set(new_centroids)))
