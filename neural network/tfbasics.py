98c788c502d9881be6f2e990a91a6bc208e566f5
rishabhshetty <rishabhshettyvm@gmail.com>
2019-05-20 21:13:59 +0530

    nn with tensorflow

--- neural network/tfbasics.py
+++ neural network/tfbasics.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
 x1 = tf.constant(5)
@@ -6,3 +8,14 @@ x2 = tf.constant(6)
 result = tf.multiply(x1,x2)
 
 print(result)
+
+##sess = tf.Session()
+##print(sess.run(result))
+##sess.close()
+
+
+with tf.Session() as sess:
+    output = sess.run(result)
+    print(output)
+##    print(sess.run(result))
+print(output) #out of a session it is a python variable 
