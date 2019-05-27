ef26a51ad2dd323424364215ea36bee12fbc95d5
rishabhshetty98 <rishabhshettyvm@gmail.com>
2019-05-28 02:08:19 +0530

    rnn

---
+++ neural network/rnn.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib import rnn
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+hm_epochs = 3
+n_classes = 10
+batch_size = 128
+chunk_size = 28
+n_chunks =28
+rnn_size = 128
+
+X = tf.placeholder('float',[None, n_chunks, chunk_size])
+y = tf.placeholder('float')
+
+def recurrent_neural_network(X):
+    layer = {'weights' :tf.Variable(tf.random_normal([rnn_size, n_classes])),
+             'biases':tf.Variable(tf.random_normal([n_classes]))}
+    X = tf.transpose(X, [1,0,2])
+    X = tf.reshape(X,[-1,chunk_size])
+    X = tf.split(X, n_chunks, 0)
+
+    lstm_cell = rnn.BasicLSTMCell(rnn_size)
+    outputs, states = rnn.static_rnn(lstm_cell, X, dtype= tf.float32)
+
+    output = tf.matmul(outputs[-1], layer['weights'])+ layer['biases']
+
+    return output
+
+def train_neural_network(x):
+    prediction = recurrent_neural_network(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):                          #training function
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x,epoch_y = mnist.train.next_batch(batch_size)
+                epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
+                _,c = sess.run([optimizer, cost], feed_dict = {x:epoch_x, y:epoch_y})
+                epoch_loss += c
+            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+
+
+
+        correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:', accuracy.eval({x:mnist.test.images.reshape((-1,n_chunks, chunk_size)), y:mnist.test.labels}))    
+    
+
+train_neural_network(X)
+
+    
